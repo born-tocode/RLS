c2b3630ba7932b783d51e957bccd7fa16eca0c53
Mateusz Boguszewski <pl.borntocode@gmail.com>
2020-09-04 12:47:34 +0200

    validation for strings, digits, and timestamp

--- src/main/java/pl/kirg/rls/domain/Product.java
+++ src/main/java/pl/kirg/rls/domain/Product.java
@@ -5,18 +5,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
 import java.math.BigDecimal;
-import java.sql.Timestamp;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity(name = "products")
@@ -29,36 +32,41 @@ public class Product
     private Long id;
 
     @NotNull
+    @Size(max = 150)
     @Column(length = 150)
     private String name;
 
-    @Column(name = "short_desc", length = 50, nullable = true)
+    @Column(name = "short_desc")
     private String shortDescription;
 
     @NotNull
-    @Column(name = "full_desc")
+    @Size(max = 8000)
+    @Column(name = "full_desc", length = 8000)
     private String fullDescription;
 
     @NotNull
-    @Column()
+    @Digits(integer = 5, fraction = 0)
     private Integer qty;
 
     @NotNull
+    @Digits(integer = 2, fraction = 0)
     @Column(name = "availability_h")
     private Integer availabilityInHours;
 
     @NotNull
-    @NumberFormat
+    @NumberFormat(style = NumberFormat.Style.CURRENCY)
     @Column(precision = 6, scale = 2)
     private BigDecimal price;
 
-    @Column(name = "discount_Pc", nullable = true)
-    private Integer discountPercent;
+    @NumberFormat(style = NumberFormat.Style.PERCENT)
+    @Digits(integer = 2, fraction = 0, message = "Max 99 percent")
+    @Column(name = "discount_Pc", precision = 2)
+    private BigDecimal discountPercent;
 
-    @DateTimeFormat
     @CreationTimestamp
+    @Temporal(TemporalType.DATE)
     @Column(updatable = false)
-    private final Timestamp timestamp;
+    private final Date registered;
 
     @Override
     public String toString()
@@ -72,7 +80,7 @@ public class Product
                ", availabilityInHours=" + availabilityInHours +
                ", price=" + price +
                ", discountPercent=" + discountPercent +
-               ", timestamp=" + timestamp +
+               ", registered=" + registered +
                '}';
     }
 }
